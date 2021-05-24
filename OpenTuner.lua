5624402acda8ee699597cb629c19184acc237952
Eric Tetz <EricTetz@users.noreply.github.com>
2021-05-24 16:23:02 -0600

    Update OpenTuner.lua
    Get rid of mute option. Now toggles tuner on/off.

--- OpenTuner.lua
+++ OpenTuner.lua
@@ -1,7 +1,5 @@
 local input = 0 -- which mono input to monitor (zero offset: 0 = first, 1 = second, ...)
 local tuner = "ReaTune" -- name of tuner effect to use
-local muteWhenTuning = false -- auto mute other tracks until tuner is closed
-local windowSize = 30 -- TODO: set "window size" parameter; only applies if using ReaTune
 local tunerTrackName = "tuner"
 
 function findTunerTrack()
@@ -24,50 +22,25 @@ function createTunerTrack()
   reaper.SetMediaTrackInfo_Value(track, "B_MAINSEND", 0) -- disable master send
   reaper.SetMediaTrackInfo_Value(track, "I_RECINPUT", input) -- choose input channel
   reaper.TrackFX_AddByName(track, tuner, false, 1)
-  if tuner == "ReaTune" then
-    
-  end
   return track
 end
 
-function exclusiveSoloTunerTrack(tunerTrack)
-  local savedSoloStates = {}
-  for t=0,reaper.CountTracks(0)-1 do
-    local track = reaper.GetTrack(0,t)
-    savedSoloStates[track] = reaper.GetMediaTrackInfo_Value(track, "I_SOLO")
-    reaper.SetMediaTrackInfo_Value(track, "I_SOLO", track == tunerTrack and 1 or 0)
-  end
-  return savedSoloStates
-end
-
-function restoreSoloStates(states)
-  for track, state in pairs(states) do
-    reaper.SetMediaTrackInfo_Value(track, "I_SOLO", state);
-  end
-end
-
-function monitorTunerWindow(tunerTrack)
-  local savedSoloStates = exclusiveSoloTunerTrack(tunerTrack)
-  local function monitor()
-      local window = reaper.TrackFX_GetFloatingWindow(tunerTrack, 0)
-      if window then
-        reaper.runloop(monitor)
-      else
-        restoreSoloStates(savedSoloStates);
-      end
+function toggleTunerTrack()
+  local track = findTunerTrack()
+  if track then
+    if reaper.TrackFX_GetFloatingWindow(track, 0) then
+       reaper.DeleteTrack(track)
+       return
     end
-  reaper.runloop(monitor)
-end
-
-function showTunerTrack()
-  local track = findTunerTrack() or createTunerTrack()
-  reaper.SetMediaTrackInfo_Value(track, "I_RECARM", 1) -- record arm
-  reaper.TrackFX_Show(track, 0, 3)
-  if muteWhenTuning then
-    monitorTunerWindow(track)
+    reaper.TrackFX_Show(track, 0, 3)
+  else
+    track = createTunerTrack()
+    reaper.TrackFX_Show(track, 0, 3)
+    reaper.SetCursorContext(0, null)
   end
+  reaper.SetMediaTrackInfo_Value(track, "I_RECARM", 1) -- record arm
 end
 
 reaper.Undo_BeginBlock()
-showTunerTrack()
+toggleTunerTrack()
 reaper.Undo_EndBlock("Open tuner", 0)
